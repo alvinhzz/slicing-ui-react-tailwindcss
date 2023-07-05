3af615e28fb44b81949daf5641c8b739ac356ad6
Aslamadin Alvian Haz <alvianaslamadin@gmail.com>
2023-07-05 14:06:35 +0700

    add image assets and create a basic navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,9 +1,44 @@
 import React from "react";
+import NavbarBrand from "../assets/img/brand-logo.svg";
 
 const Navbar = () => {
   return (
-    <div className="">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="w-full h-screen bg-[#F5F5F5] px-10">
+      <nav className="flex justify-between">
+        <div className="px-3 py-3 flex justify-start">
+          <img className="w-3/6" src={NavbarBrand} alt="" />
+        </div>
+
+        <div className="select-point">
+          <ul className="flex justify-end py-3 mx-3">
+            <li>
+              <a className="link-1" href="#">
+                CATALOGUE
+              </a>
+            </li>
+            <li>
+              <a className="link-1" href="#">
+                FASHION
+              </a>
+            </li>
+            <li>
+              <a className="link-1" href="#">
+                FAVOURITE
+              </a>
+            </li>
+            <li>
+              <a className="link-1" href="#">
+                LIFESTYLE
+              </a>
+            </li>
+            <li>
+              <a className="btn-1" href="#">
+                SIGN UP
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 };
